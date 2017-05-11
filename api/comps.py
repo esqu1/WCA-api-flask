64053cf3d549ae3949f584d6827865dcc8dc7e93
esqu1 <brandbest1@gmail.com>
2017-05-10 23:42:51 -0400

    unicode encoding problem fixed

--- api/comps.py
+++ api/comps.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+import json
 from api import app
 import os
 import MySQLdb
@@ -9,7 +10,29 @@ cur = myConnection.cursor()
 @app.route('/comps/<int:id>', methods=['GET'])
 def get_comp(id):    
     cur.execute('SELECT * FROM Competitions')
-    s = cur.fetchall()    
-    return jsonify({'result': s[id]})
+    s = cur.fetchall()
+    data = s[id]
+    new_data = []
+    for el in data:
+        try:
+            new_data.append(str(unicode(el,'latin-1').encode('utf8')))
+        except TypeError:
+            new_data.append(str(el))
+    return jsonify({'result': new_data})
+    
 
-#@app.route('/comps', methods=['GET'])
+@app.route('/comps', methods=['GET'])
+def get_all():
+    cur.execute('SELECT * FROM Competitions')
+    s = cur.fetchall()
+    new_list = []
+    for el in s:
+        data = el
+        new_data = []
+        for l in data:
+            try:
+                new_data.append(str(unicode(l,'latin-1').encode('utf8')))
+            except TypeError:
+                new_data.append(str(l))
+        new_list.append(new_data)
+    return jsonify({'result': new_list})
