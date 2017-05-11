54a67f0137ee51926304018f6b83b2322b5cc4dc
esqu1 <brandbest1@gmail.com>
2017-05-10 22:21:30 -0400

    added hostname for comps

--- api/comps.py
+++ api/comps.py
@@ -3,11 +3,13 @@ from api import app
 import os
 import MySQLdb
 
+myConnection = MySQLdb.connect(host=os.getenv('SQL_HOST'), user=os.getenv('SQL_USERNAME'), passwd=os.getenv('SQL_PASSWORD'), db=os.getenv('SQL_WCA_DB'))
+cur = myConnection.cursor()
 
 @app.route('/comps/<int:id>', methods=['GET'])
-def get_comps(id):
-    myConnection = MySQLdb.connect(user=os.getenv('SQL_USERNAME'), passwd=os.getenv('SQL_PASSWORD'), db=os.getenv('SQL_WCA_DB'))
-    cur = myConnection.cursor()
+def get_comp(id):    
     cur.execute('SELECT * FROM Competitions')
     s = cur.fetchall()    
     return jsonify({'result': s[id]})
+
+#@app.route('/comps', methods=['GET'])
