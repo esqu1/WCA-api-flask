0e689b6be5a99d6dad8c053745a9aafbdee26f02
esqu1 <brandbest1@gmail.com>
2017-05-11 13:03:20 -0400

    tuple changed to dict

--- api/comps.py
+++ api/comps.py
@@ -4,22 +4,38 @@ from api import app
 import os
 import MySQLdb
 
-myConnection = MySQLdb.connect(host=os.getenv('SQL_HOST'), user=os.getenv('SQL_USERNAME'), passwd=os.getenv('SQL_PASSWORD'), db=os.getenv('SQL_WCA_DB'))
+myConnection = MySQLdb.connect(
+    host=os.getenv('SQL_HOST'),
+    user=os.getenv('SQL_USERNAME'),
+    passwd=os.getenv('SQL_PASSWORD'),
+    db=os.getenv('SQL_WCA_DB'))
 cur = myConnection.cursor()
 
+COMP_KEYS = [
+    'id', 'name', 'cityName', 'countryId', 'information', 'year', 'month',
+    'day', 'endMonth', 'endDay', 'eventSpecs', 'wcaDelegate', 'organiser',
+    'venue', 'venueAddress', 'venueDetails', 'external_website', 'cellName',
+    'latitude', 'longitude'
+]
+
+
+def convert_to_dict(tup):
+    return {key: value for (key, value) in zip(COMP_KEYS, tup)}
+
+
 @app.route('/comps/<int:id>', methods=['GET'])
-def get_comp(id):    
+def get_comp(id):
     cur.execute('SELECT * FROM Competitions')
     s = cur.fetchall()
     data = s[id]
     new_data = []
     for el in data:
         try:
-            new_data.append(str(unicode(el,'latin-1').encode('utf8')))
+            new_data.append(str(unicode(el, 'latin-1').encode('utf8')))
         except TypeError:
             new_data.append(str(el))
-    return jsonify({'result': new_data})
-    
+    return jsonify({'result': convert_to_dict(new_data)})
+
 
 @app.route('/comps', methods=['GET'])
 def get_all():
@@ -31,8 +47,8 @@ def get_all():
         new_data = []
         for l in data:
             try:
-                new_data.append(str(unicode(l,'latin-1').encode('utf8')))
+                new_data.append(str(unicode(l, 'latin-1').encode('utf8')))
             except TypeError:
                 new_data.append(str(l))
-        new_list.append(new_data)
+        new_list.append(convert_to_dict(new_data))
     return jsonify({'result': new_list})
